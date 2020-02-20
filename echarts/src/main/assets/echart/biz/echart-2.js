93c4bf619bb806d5532f65786f7d01da4bc71a3c
AmorYin <yinzhao@newscctv.cn>
2020-02-20 17:49:40 +0800

    更新最新的echats插件

--- echarts/src/main/assets/echart/biz/echart-2.js
+++ echarts/src/main/assets/echart/biz/echart-2.js
@@ -1,173 +1,192 @@
+    /**
+     * 使用百度前度Echart框架http://echarts.baidu.com/、https://gitee.com/free/ECharts
+     * Created by amoryin on 2020/2/9.
+     */
 
-
-$(function() {
-    load();
-});
-
-function toast(msg){
-    Android.showToast(msg);
-}
-
-function init() {
-    var chartDoc = document.getElementById('chart');
-    var height = window.innerHeight;
-    $(chartDoc).css('height', height);
-
-    chart = echarts.init(chartDoc);
-    chart.clear();
-    chart.showLoading({
-        text: "Loading..."
+    $(function() {
+        init();
     });
-}
 
-function randomData() {
-    return Math.round(Math.random()*1000);
-}
+    var myChart;
 
-function load() {
-    option = {
-        title: {
-            text: 'iphone销量',
-            subtext: '纯属虚构',
-            left: 'center'
-        },
-        tooltip: {
-            trigger: 'item'
-        },
-        legend: {
-            orient: 'vertical',
-            left: 'left',
-            data:['iphone3','iphone4','iphone5']
-        },
-        visualMap: {
-            min: 0,
-            max: 2500,
-            left: 'left',
-            top: 'bottom',
-            text: ['高','低'],           // 文本，默认为数值文本
-            calculable: true
-        },
-        toolbox: {
-            show: true,
-            orient: 'vertical',
-            left: 'right',
-            top: 'center',
-            feature: {
-                dataView: {readOnly: false},
-                restore: {},
-                saveAsImage: {}
+    function init() {
+        getMyChart();
+        showChartLoading();
+        var echartJson = initChartOptions();
+        loadEcharts(echartJson);
+    }
+
+    function initChartOptions() {
+        //必须加JOSN.parse 转换数据类型
+        var echartJson = Android.getChartOptions();
+        return echartJson;
+    }
+
+    function getMyChart() {
+        if(typeof myChart !== 'undefined') {return myChart;}
+//        showDebugMessage("initEchartView");
+        var myChartDoc = document.getElementById('myChart');
+        /*
+         *var height = document.documentElement.clientHeight;
+         *var height = window.innerHeight
+         */
+        var height = window.innerHeight;
+//        showDebugMessage("height" + height.toString());
+        $(myChartDoc).css('height', height);
+        myChart = echarts.init(myChartDoc);
+        return myChart;
+    }
+
+    function showDebugMessage(msg){
+        if (typeof Android !== 'undefined') {
+           Android && Android.showMessage(msg);
+        }
+    }
+
+    /**
+	 * 构建动态图表
+	 */
+    function loadEcharts(echartJson) {
+//        showDebugMessage("loadEcharts");
+        //clear
+        getMyChart().clear();
+        //show
+        var option = JSON.parse(echartJson);
+        option = preTask(option);
+        getMyChart().setOption(option);
+        hideChartLoading();
+    }
+
+    /*
+     *刷新图表
+     */
+    function refreshEchartsWithOption(echartJson) {
+        //refresh
+        showChartLoading();
+        getMyChart().clear();
+        if(echartJson === 'null'){
+            echartJson = initChartOptions();
+        }
+        var option = JSON.parse(echartJson);
+        option = preTask(option);
+        getMyChart().setOption(option, true);//刷新，带上第二个参数true
+        hideChartLoading();
+    }
+
+    /*
+     *  页面元素加载完毕
+     */
+    function onEchartViewLoaded() {
+//        showDebugMessage("onEchartViewLoaded");
+        if (typeof Android !== 'undefined') {
+           Android && Android.chartViewLoaded();
+        }
+    }
+    /*
+     *  页面元素加载开始
+     */
+    function onEchartViewLoading() {
+//        showDebugMessage("onEchartViewLoading");
+        if (typeof Android !== 'undefined') {
+           Android && Android.chartViewLoading();
+        }
+    }
+
+    /*
+     *添加图表事件响应监听
+     */
+    function addEchartActionHandler(eventName) {
+//        showDebugMessage("addEchartActionHandler:" + eventName);
+        getMyChart().on(eventName, onEchartViewAction);
+    }
+    function onEchartViewAction(action) {
+//        showDebugMessage("onEchartViewAction:" + action.event);
+//        console.log(parseToString(action));
+        if (typeof Android !== 'undefined') {
+            if (typeof action.event !== 'undefined') {
+                Android && Android.onEChartActionEventResponse(action.event.type,parseToString(action.data));
+            }else{
+                Android && Android.onEChartActionEventResponse(action.type,parseToString(action));
             }
-        },
-        series: [
-            {
-                name: 'iphone3',
-                type: 'map',
-                mapType: 'china',
-                roam: false,
-                label: {
-                    normal: {
-                        show: true
-                    },
-                    emphasis: {
-                        show: true
-                    }
-                },
-                data:[
-                    {name: '北京',value: randomData() },
-                    {name: '天津',value: randomData() },
-                    {name: '上海',value: randomData() },
-                    {name: '重庆',value: randomData() },
-                    {name: '河北',value: randomData() },
-                    {name: '河南',value: randomData() },
-                    {name: '云南',value: randomData() },
-                    {name: '辽宁',value: randomData() },
-                    {name: '黑龙江',value: randomData() },
-                    {name: '湖南',value: randomData() },
-                    {name: '安徽',value: randomData() },
-                    {name: '山东',value: randomData() },
-                    {name: '新疆',value: randomData() },
-                    {name: '江苏',value: randomData() },
-                    {name: '浙江',value: randomData() },
-                    {name: '江西',value: randomData() },
-                    {name: '湖北',value: randomData() },
-                    {name: '广西',value: randomData() },
-                    {name: '甘肃',value: randomData() },
-                    {name: '山西',value: randomData() },
-                    {name: '内蒙古',value: randomData() },
-                    {name: '陕西',value: randomData() },
-                    {name: '吉林',value: randomData() },
-                    {name: '福建',value: randomData() },
-                    {name: '贵州',value: randomData() },
-                    {name: '广东',value: randomData() },
-                    {name: '青海',value: randomData() },
-                    {name: '西藏',value: randomData() },
-                    {name: '四川',value: randomData() },
-                    {name: '宁夏',value: randomData() },
-                    {name: '海南',value: randomData() },
-                    {name: '台湾',value: randomData() },
-                    {name: '香港',value: randomData() },
-                    {name: '澳门',value: randomData() }
-                ]
-            },
-            {
-                name: 'iphone4',
-                type: 'map',
-                mapType: 'china',
-                label: {
-                    normal: {
-                        show: true
-                    },
-                    emphasis: {
-                        show: true
-                    }
-                },
-                data:[
-                    {name: '北京',value: randomData() },
-                    {name: '天津',value: randomData() },
-                    {name: '上海',value: randomData() },
-                    {name: '重庆',value: randomData() },
-                    {name: '河北',value: randomData() },
-                    {name: '安徽',value: randomData() },
-                    {name: '新疆',value: randomData() },
-                    {name: '浙江',value: randomData() },
-                    {name: '江西',value: randomData() },
-                    {name: '山西',value: randomData() },
-                    {name: '内蒙古',value: randomData() },
-                    {name: '吉林',value: randomData() },
-                    {name: '福建',value: randomData() },
-                    {name: '广东',value: randomData() },
-                    {name: '西藏',value: randomData() },
-                    {name: '四川',value: randomData() },
-                    {name: '宁夏',value: randomData() },
-                    {name: '香港',value: randomData() },
-                    {name: '澳门',value: randomData() }
-                ]
-            },
-            {
-                name: 'iphone5',
-                type: 'map',
-                mapType: 'china',
-                label: {
-                    normal: {
-                        show: true
-                    },
-                    emphasis: {
-                        show: true
-                    }
-                },
-                data:[
-                    {name: '北京',value: randomData() },
-                    {name: '天津',value: randomData() },
-                    {name: '上海',value: randomData() },
-                    {name: '广东',value: randomData() },
-                    {name: '台湾',value: randomData() },
-                    {name: '香港',value: randomData() },
-                    {name: '澳门',value: randomData() }
-                ]
-            }
-        ]
-    };
+        }
+    }
 
-    chart.hideLoading();
-    chart.setOption(option);
-}
+    /*
+     *移除图表事件响应监听
+     */
+    function removeEchartActionHandler(eventName) {
+//        showDebugMessage("removeEchartActionHandler:" + eventName);
+        getMyChart().un(name, removeEchartViewAction);
+    }
+    function removeEchartViewAction(action) {
+//        showDebugMessage("removeEchartViewAction:" + action.event.type);
+        if (typeof Android !== 'undefined') {
+            Android && Android.removeEChartActionEventResponse(action.event.type);
+        }
+    }
+
+    function showChartLoading() {
+        getMyChart().showLoading();
+        onEchartViewLoading();
+    }
+    function hideChartLoading() {
+        getMyChart().hideLoading();
+        onEchartViewLoaded();
+    }
+
+
+    //util
+    function preTask(obj) {
+        var result;
+        if(typeof(obj) == 'object') {
+            if(obj instanceof Array) {
+                result = new Array();
+                for (var i = 0, len = obj.length; i < len ; i++) {
+                     result.push(preTask(obj[i]));
+                }
+                return result;
+            } else if(obj instanceof RegExp){
+                return obj;
+            } else {
+                result = new Object();
+                for (var prop in obj) {
+                    result[prop] = preTask(obj[prop]);
+                }
+                return result;
+            }
+        } else if(typeof(obj) == 'string'){
+            try {
+                if(typeof(eval(obj)) == 'function'){
+                    return eval(obj);
+                } else if (typeof(eval(obj) == 'object') && (eval(obj) instanceof Array || eval(obj) instanceof CanvasGradient)) {
+                    return eval(obj);
+                }
+            }catch(e) {
+                return obj;
+            }
+            return obj;
+        } else if(typeof(obj) == 'undefined'){
+            showDebugMessage("数据格式错误");
+            return obj;
+        } else {
+            return obj;
+        }
+    }
+
+    //option 转 string
+    function parseToString(o){
+        var cache = [];
+        var str = JSON.stringify(o, function(key, value) {
+            if (typeof value === 'object' && value !== null) {
+                if (cache.indexOf(value) !== -1) {
+                    // 移除
+                    return;
+                }
+                // 收集所有的值
+                cache.push(value);
+            }
+            return value;
+        });
+        cache = null; // 清空变量，便于垃圾回收机制回收
+        return str;
+    }
